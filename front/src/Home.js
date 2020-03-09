86a068ed36aab1c3910f79100d8a592b36523b26
FlorianLz <46797434+florianlz@users.noreply.github.com>
2020-03-09 16:27:50 +0100

    Avancement App.js.

--- front/src/Home.js
+++ front/src/Home.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import QuizzThumbnail from "./QuizzThumbnail";
+import axios from 'axios';
 
 function Home()  {
-
+    const [quizz, setQuizz] = useState([]);
+    async function getQuizzes() {
+        const data = (await axios.get('http://localhost:8000/quizzs')).data;
+        setQuizz(data);
+    }
+    useEffect(() => {
+        getQuizzes()
+    },[]);
     return (
         <div>Home Page <br /><br />
             <QuizzThumbnail title={"Dora l'exploratrice"} date={"06/05/2000"} author={"auteur"} />
