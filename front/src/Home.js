6527ffc291b889b159a39c732445813e47caa162
FlorianLz <46797434+florianlz@users.noreply.github.com>
2020-03-09 16:45:17 +0100

    Récupération de la liste des quizz.

--- front/src/Home.js
+++ front/src/Home.js
@@ -3,18 +3,27 @@ import QuizzThumbnail from "./QuizzThumbnail";
 import axios from 'axios';
 
 function Home()  {
-    const [quizz, setQuizz] = useState([]);
+    const [quizzes, setQuizzes] = useState([]);
+
+    let jsxQuizzes = quizzes.map(p => <QuizzThumbnail
+        id = {p.id}
+        title={p.name}
+        date={p.date}
+        author={p.author}
+        image={p.picture_url}/>)
+
     async function getQuizzes() {
-        const data = (await axios.get('http://localhost:8000/quizzs')).data;
-        setQuizz(data);
+        const data = (await axios.get('http://localhost:8000/quizz')).data;
+        setQuizzes(data);
     }
+
     useEffect(() => {
         getQuizzes()
     },[]);
+
     return (
         <div>Home Page <br /><br />
-            <QuizzThumbnail title={"Dora l'exploratrice"} date={"06/05/2000"} author={"auteur"} />
-            <QuizzThumbnail title={"L'âne trotro"} date={"06/05/2000"} author={"auteur"} />
+            {jsxQuizzes}
         </div>
 
     );
