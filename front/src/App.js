19ffd3f1d3fd63e254ef1b68218e9264faafd619
LydieV <49896819+LydieV@users.noreply.github.com>
2020-03-09 16:57:28 +0100

    Creation fichier login.

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,7 @@ import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import Home from "./Home.js";
 import About from "./About.js";
+import Login from "./Login.js";
 
 function App()  {
     return (
@@ -14,6 +15,7 @@ function App()  {
                 <Switch>
                     <Route exact={true} path="/" component={Home} />
                     <Route exact={true} path="/about" component={About} />
+                    <Route exact={true} path="/login" component={Login} />
                     <Route path="*" component={() => <p>Page Not Found</p>} />
                 </Switch>
             </div>
