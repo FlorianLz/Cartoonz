f17fa7b656585a2335e3988c0341449470ed843c
FlorianLz <46797434+florianlz@users.noreply.github.com>
2020-03-09 15:55:44 +0100

    Début création server.js et router.js

--- front/src/QuizzThumbnail.js
+++ front/src/QuizzThumbnail.js
@@ -2,13 +2,15 @@ import React from "react";
 
 export default function QuizzThumbnail(props)  {
     return (
-        <div>
-            Title : {props.title}
-            <br />
-            Date : {props.date}
-            <br />
-            Author : {props.author}
-            <br /><br />
+        <div className={"quizz_card"}>
+            <div className="image">
+
+            </div>
+            <div className="infos">
+                <p>Title : {props.title}</p>
+                <p>Published on {props.date}</p>
+                <p>by {props.author}</p>
+            </div>
         </div>
     );
 }
