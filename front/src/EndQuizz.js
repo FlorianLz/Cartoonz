4a113b9b01d560a1d2c3a22c5bc10168b3e62ad4
LydieV <49896819+LydieV@users.noreply.github.com>
2020-03-11 19:24:27 +0100

    Modif.

--- front/src/EndQuizz.js
+++ front/src/EndQuizz.js
@@ -7,7 +7,7 @@ export default function EndQuizz(props)  {
             <h2 className="quizzName">{props.nomquizz}</h2>
             <img src={'http://localhost:8000/'+props.picture} className="imgfinquizz" alt="Picture of the quizz."/>
             <p className="sentencefinish"> Congratulations, you have finish the quizz with a score of : <span className="scoreFinal">{props.score}</span> points.</p>
-            <p className="sentencefinish"> You can play at an other quizz on the <Link to={'/'}><div className="lienFinish"> Home Page</div> </Link>.</p>
+            <p className="sentencefinish"> You can play at an other quizz on the <Link to={'/'}><div className="lienFinish"> Home Page</div> </Link></p>
         </div>
 
     );
